ad2dc42bc18bac5c6e1da772d92003b12951d34b
nourhanemadd <121579831+nourhanemadd@users.noreply.github.com>
2022-12-29 00:02:20 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
+Console.WriteLine("Please enter the first number: ");
+int start= int.Parse(Console.ReadLine());
+
+Console.WriteLine("Please enter the second number: ");
+int end = int.Parse(Console.ReadLine());
+
+Console.WriteLine("The prime numbers between them are :");
+
+int sum = 0;
+for (int i = start; i <= end; i++)
+{
+    
+    for (int x = 1; x <= i; x++)
+    {
+        if (i%x ==0)
+        {
+            sum++;
+
+        }
+        
+    }
+    if (sum == 2)
+    {
+        Console.WriteLine(i);
+        sum = 0;
+    }
+    else
+    {
+        sum = 0;
+        continue;
+
+    }
+}
